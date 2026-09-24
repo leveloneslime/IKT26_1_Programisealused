402d31566b5c1c772ecf14755b81c7ebe48e1455
Opilane Techno+TLN <siroakingtonthefirst@gmail.com>
2026-09-24 10:53:33 +0300

    meetodikutsumine

---
+++ 04.24.09.2026/IfElseMethodCall/IfElseMethodCall/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IfElseMethodCall
+{
+    internal class Program
+    {
+        //main on meetod, mis läheb alati esimesena tööle
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vali meetod mida kasutad");
+            string Method = Console.ReadLine();
+            if (Method == "HelloMethod")
+            {
+                //kui kirjutad meetodi nime siis seda nimetatakse
+                //meetodi kutsumiseks 
+                HelloMethod();
+            }
+            else
+            {
+                Console.WriteLine("kahtlane meetod");
+            }
+        }
+
+        //teha teine meetod siia ja nimeks sellel HelloMethod
+
+        static void HelloMethod()
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
