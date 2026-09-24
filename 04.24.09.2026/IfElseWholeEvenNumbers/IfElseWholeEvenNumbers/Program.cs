75f2d0b3029c143d1dd1766728d71608f36b5481
Opilane Techno+TLN <siroakingtonthefirst@gmail.com>
2026-09-24 11:17:39 +0300

    IfElseWholeOrEven

---
+++ 04.24.09.2026/IfElseWholeEvenNumbers/IfElseWholeEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IfElseWholeEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //konsool küsib numbrit
+            //number tuleb ära parsida
+
+            //if ja else juures toimub kontroll
+            //kas on paaris või paaritu
+
+            Console.WriteLine("kirjuta number");
+
+            string number = Console.ReadLine();
+            int numberInt = int.Parse(number);
+
+            if (numberInt%2 == 0)
+            {
+                Console.WriteLine(number + " is even");
+            }
+            else
+            {
+                Console.WriteLine(number + " is odd");
+            }
+
+        }
+    }
+}
