ac255bac3787185dade52e256c5458aecb0eaa27
Opilane Techno+TLN <siroakingtonthefirst@gmail.com>
2026-09-24 10:05:03 +0300

    elseifautod

---
+++ 04.24.09.2026/IfElseautod/IfElseautod/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.Globalization;
+
+namespace IfElseautod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisestage oma automark");
+            //kasuta if else
+            //kirjuta automark
+            //kalikus on BMW, Audi, Porsche, Fiat ja skoda
+            //kui valitakse skoda, siis seal sees on uuesti küsimus, et
+            //miss mudel soovid valida. mudeli valikus on Kodiaq ja Octavia
+
+            string auto = Console.ReadLine();
+
+
+            if (auto == "BMW")
+            {
+                Console.WriteLine("auto on bmw");
+            }
+            else if (auto == "audi")
+            {
+                Console.WriteLine("auto on audi");
+            }
+            else if (auto == "porsche")
+            {
+                Console.WriteLine("auto on porsche");
+            }
+            else if (auto == "fiat")
+            {
+                Console.WriteLine("auto on fiat");
+            }
+            else if (auto == "skoda")
+            {
+                Console.WriteLine("Palun vali auto mudel");
+
+                string mudel = Console.ReadLine();
+                if (mudel == "kodiaq")
+                {
+                    Console.WriteLine("teie auto on skoda kodiaq");
+                }
+                else if (mudel == "octavia")
+                {
+                    Console.WriteLine("teie auto on skoda Octavia");
+                }
+                else
+                {
+                    Console.WriteLine("kahtlane mudel");
+                }
+            }
+            else
+            {
+                Console.WriteLine("kahtlane auto");
+            }
+
+        }
+    }
+}
